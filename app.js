34047deee3715662b06c3c88b4ba164c99ed3f0b
passomvnu <passomanu92@gmail.com>
2026-08-24 10:29:48 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -8,8 +8,8 @@
 // ==========================================================================
 const CLINICA_CONFIG = {
   // Formato internacional de Argentina: 549 + 11 + número de celular sin el 15
-  whatsappNumber: '5491122467973', // Número de WhatsApp para Turnos y Consultas
-  telefonoGuardia: '011 4264-1654',
+  whatsappNumber: '5491139511478', // Número de WhatsApp para Turnos y Consultas
+  telefonoGuardia: '011 42640056',
   direccion: 'Av. Eva Perón 3097, B1832 Temperley, Provincia de Buenos Aires'
 };
 
